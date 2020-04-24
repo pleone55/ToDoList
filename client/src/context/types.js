cc1dd1577ae53ba36b799f56c2a0865a9f8cfb0f
Paul Leone <35977957+pleone55@users.noreply.github.com>
2020-04-23 20:34:05 -0700

    added CRUD types to be implemented for the Context API state management

---
+++ client/src/context/types.js
@@ -0,0 +1,11 @@
+//CRUD
+export const GET_TASKS = 'GET_TASKS';
+export const CLEAR_TASKS = 'CLEAR_TASKS';
+export const ADD_TASK = 'ADD_TASK';
+export const TASK_ERROR = 'TASK_ERROR';
+export const DELETE_TASK = 'DELETE_TASK';
+export const SET_CURRENT = 'SET_CURRENT';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT';
+export const UPDATE_TASK = 'UPDATE_TASK';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
